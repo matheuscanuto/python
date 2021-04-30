828bf5d4d20ddbba8bd2deaf90c4d6198639560d
matheuscanuto <83411828+matheuscanuto@users.noreply.github.com>
2021-04-30 11:41:14 -0300

    python files

--- python/connect.py
+++ python/connect.py
@@ -5,6 +5,6 @@ cursor=banco.cursor()
 
 cursor.execute("CREATE TABLE people (name text,  age integer, email text)")
 
-cursor.execute("INSERT INTO peoplghe VALUES('jason',40,'jason_13@gmail.com')")
+cursor.execute("INSERT INTO people VALUES('jason',40,'jason_13@gmail.com')")
 
 banco.commit()
