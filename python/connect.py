73f493b49aab48eabfd009c1dc389f1851d18d51
matheuscanuto <83411828+matheuscanuto@users.noreply.github.com>
2021-04-30 11:40:11 -0300

    Update connect.py

--- python/connect.py
+++ python/connect.py
@@ -3,8 +3,8 @@ import sqlite3
 banco=sqlite3.connect('first_bank.db')
 cursor=banco.cursor()
 
-cursor.execute("CREATE TABLE pessoas (name text,  age integer, email text)")
+cursor.execute("CREATE TABLE people (name text,  age integer, email text)")
 
-cursor.execute("INSERT INTO pessoas VALUES('jason',40,'jason_13@gmail.com')")
+cursor.execute("INSERT INTO people VALUES('jason',40,'jason_13@gmail.com')")
 
 banco.commit()
