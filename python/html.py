49257860971cf0c7196cf83832c8427aec0102bd
matheuscanuto <83411828+matheuscanuto@users.noreply.github.com>
2021-05-03 18:43:26 -0300

    python files

---
+++ python/html.py
@@ -0,0 +1,5 @@
+archive = open('archive.html','w') #I named it as file.html, but you can put any other name
+archive.write("<html> \n")
+archive.write("<h1>HELLO WORLD</h1>\n")
+archive.write("</html>") 
+archive.close()
