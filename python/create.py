d2547e3485fc8ed803639b991d679dbad3ebb87e
matheuscanuto <83411828+matheuscanuto@users.noreply.github.com>
2021-05-03 18:38:29 -0300

    python files

--- python/create.py
+++ python/create.py
@@ -1,3 +1,3 @@
 archive = open('archive.txt','w') #I named it as file.txt, but you can put any other name
-archive.write("olá mundo") #the write command is where you are going to write on a line, and to break the line you put \ n at the end and put another .write and put other content, and so on
+archive.write("hello world !") #the write command is where you are going to write on a line, and to break the line you put \ n at the end and put another .write and put other content, and so on
 archive.close() #closes the file
